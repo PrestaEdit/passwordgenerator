838de892590e83151f5f337d2a29bd00a8d9344e
Jonathan Danse <j.danse@prestaedit.com>
2023-04-26 22:13:37 +0200

    Adapt module to PrestaShop 1.7.6 and higher
    - Remove CoreClass
    - Remove everyting related to previous remove
    - Use FormModifier to alter Customer form
    - Manage datas in different way
    - Add password template in order to get variables inside it (not present in Moden theme)
    - Remove Addons related
    - Bump version

--- passwordgenerator.php
+++ passwordgenerator.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 2007-2016 PrestaShop
+* 2007-2023 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2016 PrestaShop SA
+*  @copyright 2007-2023 PrestaShop SA
 *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -28,21 +28,17 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-if (!class_exists('PrestaEditCoreClass')) {
-    include_once _PS_ROOT_DIR_ . '/modules/passwordgenerator/PrestaEditCoreClass.php';
-}
 
-class PasswordGenerator extends PrestaEditCoreClass
+class PasswordGenerator extends Module
 {
     public function __construct()
     {
         $this->name = 'passwordgenerator';
         $this->tab = 'administration';
-        $this->version = '1.2.5';
+        $this->version = '1.3.0';
         $this->author = 'PrestaEdit';
         $this->ps_versions_compliancy = array('min' => '1.5.0.1', 'max' => _PS_VERSION_);
         $this->need_instance = 0;
-        $this->module_key = '59c64ca74a5fa7a97d17b9ebc4746c7f';
 
         parent::__construct();
 
@@ -58,18 +54,30 @@ class PasswordGenerator extends PrestaEditCoreClass
             return false;
         }
 
-        // Customer
-        if (!$this->registerHook('actionObjectCustomerAddAfter') || !$this->registerHook('actionObjectCustomerUpdateAfter')
-            || !$this->registerHook('displayAdminCustomersForm')) {
-            return false;
+        if (version_compare(_PS_VERSION_, '1.6.0.1', '<')) {
+            $hooks = [
+                'displayAdminEmployeesForm',
+                'actionObjectEmployeeAddAfter',
+                'actionObjectEmployeeUpdateAfter',
+                'actionObjectCustomerAddAfter',
+                'actionObjectCustomerUpdateAfter',
+            ];
+        } else if (version_compare(_PS_VERSION_, '1.7.6.0', '<')) {
+            $hooks = [
+                'actionObjectCustomerAddAfter',
+                'actionObjectCustomerUpdateAfter',
+            ];
+        } else {
+            $hooks = [
+                'actionAdminControllerSetMedia',
+                'actionCustomerFormBuilderModifier',
+                'actionAfterCreateCustomerFormHandler',
+                'actionAfterUpdateCustomerFormHandler',
+            ];
         }
 
-        // Employee
-        if (version_compare(_PS_VERSION_, '1.6.0.1', '<')) {
-            if (!$this->registerHook('actionObjectEmployeeAddAfter') || !$this->registerHook('actionObjectEmployeeUpdateAfter')
-                || !$this->registerHook('displayAdminEmployeesForm')) {
-                return false;
-            }
+        if (!$this->registerHook($hooks)) {
+            return false;
         }
 
         return true;
@@ -87,44 +95,145 @@ class PasswordGenerator extends PrestaEditCoreClass
 
     public function hookActionObjectCustomerAddAfter($params)
     {
-        $this->performProcess($params);
+        $this->hookActionObjectEmployeeUpdateAfter($params);
     }
 
     public function hookActionObjectCustomerUpdateAfter($params)
     {
-        $this->performProcess($params);
+        $this->hookActionObjectEmployeeUpdateAfter($params);
     }
 
     public function hookActionObjectEmployeeAddAfter($params)
     {
-        $this->performProcess($params);
+        $this->hookActionObjectEmployeeUpdateAfter($params);
     }
 
     public function hookActionObjectEmployeeUpdateAfter($params)
     {
-        $this->performProcess($params);
+        $params['password'] = Tools::getValue('passwd');
+        $this->performProcess($params, (bool) Tools::isSubmit('sendMail'));
     }
 
-    protected function performProcess($params)
+    /**
+     * Hook allows to modify Customers form and add additional form fields as well as modify or add new data to the forms.
+     *
+     * @param array $params
+     */
+    public function hookActionCustomerFormBuilderModifier(array $params)
     {
-        if (Tools::isSubmit('sendMail')) {
-            $this->sendMail($params['object'], Tools::getValue('passwd'));
+        /** @var FormBuilderInterface $formBuilder */
+        $formBuilder = $params['form_builder'];
+        $formBuilder->add('send_mail', \PrestaShopBundle\Form\Admin\Type\SwitchType::class, [
+            'label' => $this->getTranslator()->trans('Send by mail', [], 'Modules.PasswordGenerator.Displayadmincustomersform'),
+            'required' => false,
+        ]);
+        $formBuilder->add('generator', \Symfony\Component\Form\Extension\Core\Type\ButtonType::class, [
+            'label' => $this->getTranslator()->trans('Generate code', [], 'Modules.PasswordGenerator.Displayadmincustomersform'),
+            'row_attr' => ['class' => 'row'],
+            'attr' => [
+                'class' => 'offset-4 col-sm-3 input-container btn btn-default',
+                'id' => 'passwd-generate-field',
+                'onclick' => 'passwordGeneratorGenerateCode(10);',
+            ],
+        ]);
+
+        $params['data']['send_mail'] = false;
+
+        $formBuilder->setData($params['data']);
+    }
+
+    /**
+     * Hook allows to modify Customers form and add additional form fields as well as modify or add new data to the forms.
+     *
+     * @param array $params
+     *
+     * @throws CustomerException
+     */
+    public function hookActionAfterUpdateCustomerFormHandler(array $params)
+    {
+        $process = $params['form_data']['send_mail'];
+        $params['password'] = $params['form_data']['password'];
+        $this->performProcess($params, $process);
+    }
+
+    /**
+     * Hook allows to modify Customers form and add additional form fields as well as modify or add new data to the forms.
+     *
+     * @param array $params
+     *
+     * @throws CustomerException
+     */
+    public function hookActionAfterCreateCustomerFormHandler(array $params)
+    {
+        $this->hookActionAfterUpdateCustomerFormHandler($params);
+    }
+
+    public function hookActionAdminControllerSetMedia($params)
+    {
+        if ('AdminCustomers' !== $this->context->controller->controller_name) {
+            return;
         }
+
+        $action = Tools::getValue('action');
+
+        if ($action !== 'updatecustomer' && $action !== 'addcustomer') {
+            return;
+        }
+
+        $this->context->controller->addJS('modules/' . $this->name . '/views/js/customers.js');
     }
 
-    protected function sendMail($object, $passwd)
+    protected function performProcess($params, $process = false)
     {
-        $template_vars = array(
-            '{email}' => $object->email,
-            '{passwd}' => $passwd,
-            '{firstname}' => $object->firstname,
-            '{lastname}' => $object->lastname
-        );
+        if (!$process) {
+            return;
+        }
 
+        $this->sendMail($params, $params['password']);
+    }
+
+    protected function sendMail($params, $passwd)
+    {
+        if (isset($params['object']) && is_object($params)) {
+            $templateVars = array(
+                '{email}' => $params['object']->email,
+                '{passwd}' => $params['password'],
+                '{firstname}' => $params['object']->firstname,
+                '{lastname}' => $params['object']->lastname,
+            );
+            $langId = (int) $params['object']->id_lang;
+        } else {
+            $templateVars = array(
+                '{email}' => $params['form_data']['email'],
+                '{passwd}' => $params['password'],
+                '{firstname}' => $params['form_data']['first_name'],
+                '{lastname}' => $params['form_data']['last_name'],
+            );
+            $langId = (int) Context::getContext()->language->id;
+        }
+
+        $emailTo = $templateVars['{email}'];
         if (version_compare(_PS_VERSION_, '1.7.0.0', '>=')) {
-            $template_vars['{email}'] = $object->email.'<br /><span style="color:#333"><strong>'.$this->l('Password').' :</strong></span> '.$passwd;
+            $templateVars['{email}'] .= '<br /><span style="color:#333"><strong>'.$this->l('Password').' :</strong></span> '.$templateVars['{passwd}'];
         }
 
-        Mail::Send((int)$object->id_lang, 'password', Mail::l('Your new password', (int)$object->id_lang), $template_vars, $object->email, $object->firstname.' '.$object->lastname);
+        $mailPath = _PS_MAIL_DIR_;
+        if (version_compare(_PS_VERSION_, '1.7.6.0', '>=')) {
+            $mailPath = _PS_MODULE_DIR_ . $this->name . '/mails';
+        }
+
+        Mail::Send(
+            (int) $langId,
+            'password',
+            Mail::l('Your new password', (int) $langId),
+            $templateVars,
+            $emailTo,
+            $templateVars['{firstname}'].' '.$templateVars['{lastname}'],
+            null,
+            null,
+            null,
+            null,
+            $mailPath
+        );
     }
 }
